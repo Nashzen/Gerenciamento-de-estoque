d8f68636b293e0f7fc50b47077f26ed89c5d6f09
Léo Salomão <leosalomao20@gmail.com>
2020-05-31 23:10:51 -0300

    second commit

---
+++ Tcc-Senai/Tcc-Senai/Entities/StockDelivery.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TccSenai.Entities.Base;
+
+namespace TccSenai.Entities
+{
+    public class StockDelivery : EntityBase
+    {
+        public StockDelivery(DateTime estimateDate, DateTime deliveryDate, double deliveryValue, int quantityDelivered)
+        {
+            EstimateDate = estimateDate;
+            DeliveryDate = deliveryDate;
+            DeliveryValue = deliveryValue;
+            QuantityDelivered = quantityDelivered;
+        }
+
+        public DateTime EstimateDate { get; set; }
+
+        public DateTime DeliveryDate { get; set; }
+
+        public double DeliveryValue { get; set; }
+
+        public int QuantityDelivered { get; set; }
+
+        //public Provider Provider { get; set; }
+    }
+}
