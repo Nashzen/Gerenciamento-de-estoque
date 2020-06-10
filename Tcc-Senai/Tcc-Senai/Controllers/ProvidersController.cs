6953c811f7adfb4c8047599661ed14092a77082a
Léo Salomão <leosalomao20@gmail.com>
2020-06-09 23:38:36 -0300

    scaffold das entidades

---
+++ Tcc-Senai/Tcc-Senai/Controllers/ProvidersController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TCC_Senai.Models;
+using TCC_Senai.Models.Context;
+
+namespace TCC_Senai.Controllers
+{
+    public class ProvidersController : Controller
+    {
+        private Context db = new Context();
+
+        // GET: Providers
+        public ActionResult Index()
+        {
+            return View(db.Providers.ToList());
+        }
+
+        // GET: Providers/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Provider provider = db.Providers.Find(id);
+            if (provider == null)
+            {
+                return HttpNotFound();
+            }
+            return View(provider);
+        }
+
+        // GET: Providers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Providers/Create
+        // Para se proteger de mais ataques, habilite as propriedades específicas às quais você quer se associar. Para 
+        // obter mais detalhes, veja https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Cnpj,Phone,Email,CreationDate")] Provider provider)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Providers.Add(provider);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(provider);
+        }
+
+        // GET: Providers/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Provider provider = db.Providers.Find(id);
+            if (provider == null)
+            {
+                return HttpNotFound();
+            }
+            return View(provider);
+        }
+
+        // POST: Providers/Edit/5
+        // Para se proteger de mais ataques, habilite as propriedades específicas às quais você quer se associar. Para 
+        // obter mais detalhes, veja https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Cnpj,Phone,Email,CreationDate")] Provider provider)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(provider).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(provider);
+        }
+
+        // GET: Providers/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Provider provider = db.Providers.Find(id);
+            if (provider == null)
+            {
+                return HttpNotFound();
+            }
+            return View(provider);
+        }
+
+        // POST: Providers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Provider provider = db.Providers.Find(id);
+            db.Providers.Remove(provider);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Tcc-Senai/Tcc-Senai/Controllers/StocksController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TCC_Senai.Models;
+using TCC_Senai.Models.Context;
+
+namespace TCC_Senai.Controllers
+{
+    public class StocksController : Controller
+    {
+        private Context db = new Context();
+
+        // GET: Stocks
+        public ActionResult Index()
+        {
+            var stocks = db.Stocks.Include(s => s.Product);
+            return View(stocks.ToList());
+        }
+
+        // GET: Stocks/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Stock stock = db.Stocks.Find(id);
+            if (stock == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stock);
+        }
+
+        // GET: Stocks/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProductId = new SelectList(db.Products, "Id", "Name");
+            return View();
+        }
+
+        // POST: Stocks/Create
+        // Para se proteger de mais ataques, habilite as propriedades específicas às quais você quer se associar. Para 
+        // obter mais detalhes, veja https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,TotalAmmount,AvailableAmmount,ProductId,CreationDate")] Stock stock)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Stocks.Add(stock);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProductId = new SelectList(db.Products, "Id", "Name", stock.ProductId);
+            return View(stock);
+        }
+
+        // GET: Stocks/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Stock stock = db.Stocks.Find(id);
+            if (stock == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProductId = new SelectList(db.Products, "Id", "Name", stock.ProductId);
+            return View(stock);
+        }
+
+        // POST: Stocks/Edit/5
+        // Para se proteger de mais ataques, habilite as propriedades específicas às quais você quer se associar. Para 
+        // obter mais detalhes, veja https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,TotalAmmount,AvailableAmmount,ProductId,CreationDate")] Stock stock)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(stock).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProductId = new SelectList(db.Products, "Id", "Name", stock.ProductId);
+            return View(stock);
+        }
+
+        // GET: Stocks/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Stock stock = db.Stocks.Find(id);
+            if (stock == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stock);
+        }
+
+        // POST: Stocks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Stock stock = db.Stocks.Find(id);
+            db.Stocks.Remove(stock);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
