22807d40519286d3668aae79cc9e6dd512959ed9
Léo Salomão <leosalomao20@gmail.com>
2020-06-27 23:55:24 -0300

    Mudanças página de login

--- Tcc-Senai/Tcc-Senai/Controllers/HomeController.cs
+++ Tcc-Senai/Tcc-Senai/Controllers/HomeController.cs
@@ -8,6 +8,12 @@ namespace TCC_Senai.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult Login()
+        {
+            
+            return View();
+        }
+
         public ActionResult Index()
         {
             return View();
