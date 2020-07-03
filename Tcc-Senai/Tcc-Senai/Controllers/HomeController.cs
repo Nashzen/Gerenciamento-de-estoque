389197c5913bc56a925a8c77f498c228acf468c8
Léo Salomão <leosalomao20@gmail.com>
2020-07-02 21:27:55 -0300

    Validação login

--- Tcc-Senai/Tcc-Senai/Controllers/HomeController.cs
+++ Tcc-Senai/Tcc-Senai/Controllers/HomeController.cs
@@ -3,15 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using TCC_Senai.Models;
 
 namespace TCC_Senai.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Login()
+        [AllowAnonymous]
+        //Método para direcionar para a tela do login
+        public ActionResult Login(string returnUrl)
         {
-            
+            ViewBag.ReturnUrl = returnUrl;
             return View();
+        }
+
+        //Método para logar
+        [HttpPost]
+        public ActionResult Login(User login, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(login);
+            }
+
+            var valid = (login.UserName == "leo" && login.Password == "123");
+
+            if (valid)
+            {
+                FormsAuthentication.SetAuthCookie(login.UserName , login.IsActive);
+                if (Url.IsLocalUrl(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    RedirectToAction("Home", "Login");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Login Invalido.");
+            }
+            return View(login);
         }
 
         public ActionResult Index()
--- Tcc-Senai/Tcc-Senai/Models/User.cs
+++ Tcc-Senai/Tcc-Senai/Models/User.cs
@@ -1,15 +1,23 @@
-﻿using TCC_Senai.Models.Base;
+﻿using System.ComponentModel.DataAnnotations;
+using TCC_Senai.Models.Base;
 
 namespace TCC_Senai.Models
 {
     public class User : EntityBase
     {
+        [Display(Name = "Usuário")]
+        [Required(ErrorMessage = "Informe um usuário válido")]
         public string UserName { get; set; }
 
+        [Display(Name = "Senha")]
+        [Required(ErrorMessage = "Senha incorreta")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
+        [Display(Name = "ativo")]
         public bool IsActive { get; set; }
 
+        [Display(Name = "empregado")]
         public Employee Employee { get; set; }
     }
 }
